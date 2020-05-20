86634a86055ea53239008fa2529d3a63298ebf84
Felipe H. Teixeira <felipe@dinfo.com.br>
2020-05-20 00:59:32 -0300

    feat: add repositories and like routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4');
 
 const app = express();
 
@@ -10,24 +10,72 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get('/repositories', (request, response) => {
+  return response.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post('/repositories', (request, response) => {
+  const repository = {
+    id: uuid(),
+    likes: 0,
+    ...request.body
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex((repo) => repo.id === id);
+  if (repositoryIndex < 0) {
+    return response.status(400).json({
+      error: 'Repositório não encontrado!'
+    });
+  }
+
+  const repositoryToUpdate = { ...repositories[repositoryIndex] };
+  repositoryToUpdate.title = title || repositoryToUpdate.title;
+  repositoryToUpdate.url = url || repositoryToUpdate.url;
+  repositoryToUpdate.techs = techs || repositoryToUpdate.techs;
+  repositories[repositoryIndex] = repositoryToUpdate;
+
+  return response.json(repositoryToUpdate);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex((repo) => repo.id === id);
+  if (repositoryIndex < 0) {
+    return response.status(400).json({
+      error: 'Repositório não encontrado!'
+    });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post('/repositories/:id/like', (request, response) => {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex((repo) => repo.id === id);
+  if (repositoryIndex < 0) {
+    return response.status(400).json({
+      error: 'Repositório não encontrado!'
+    });
+  }
+
+  repositories[repositoryIndex].likes += 1;
+
+  return response.status(200).send({
+    likes: repositories[repositoryIndex].likes
+  });
 });
 
 module.exports = app;
